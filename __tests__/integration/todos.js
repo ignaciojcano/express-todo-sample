02d90ba9af2e0eb522a3ee2ec1ef6dba4e176712
Ignacio Cano <ignaciojcano@gmail.com>
2020-02-01 20:20:44 -0400

    lint fix

--- __tests__/integration/todos.js
+++ __tests__/integration/todos.js
@@ -7,37 +7,33 @@ describe('TODO', () => {
   beforeEach(() => {});
   afterEach(() => {});
 
-  it('should fetch all todos', async (done) => {
+  it('should fetch all todos', async () => {
     const response = await request.get('/todos');
     expect(response.status).toBe(200);
     expect(response.body).toMatchSnapshot();
-    done();
   });
 
-  it('should create a todo', async (done) => {
+  it('should create a todo', async () => {
     const response = await request.post('/todos')
       .send({
         text: 'I just got created',
       });
     expect(response.status).toBe(200);
     expect(response.body).toMatchSnapshot();
-    done();
   });
 
-  it('should fetch a todo', async (done) => {
+  it('should fetch a todo', async () => {
     const response = await request.get('/todos/1');
     expect(response.status).toBe(200);
     expect(response.body).toMatchSnapshot();
-    done();
   });
 
-  it('should update a todo', async (done) => {
+  it('should update a todo', async () => {
     const response = await request.put('/todos/1')
       .send({
         text: 'A new and updated text',
       });
     expect(response.status).toBe(200);
     expect(response.body).toMatchSnapshot();
-    done();
   });
 });
